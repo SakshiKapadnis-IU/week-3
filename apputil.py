a721fbe143407cc42a42174155d795241d69919d
SakshiKapadnis-IU <sajakapa@iu.edu>
2025-09-17 03:29:53 +0000

    week 3 done

--- apputil.py
+++ apputil.py
@@ -1,5 +1,15 @@
-import seaborn as sns
-import pandas as pd
 
-
-# update/add code below ...
+# Exercise 1: Fibonacci (Recursive)
+def fibonacci(n):
+    """
+    Recursive function to return the nth Fibonacci number.
+    """
+    # Base cases
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    # Recursive case
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
